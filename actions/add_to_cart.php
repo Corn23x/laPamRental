a45fcf1d2a430b3de60dbed7ba5de0bb7643852b
CORNELIUS APAALSE <cornelius.apaalse@ashesi.edu.gh>
2022-12-03 03:57:53 +0000

    Cart Management

--- actions/add_to_cart.php
+++ actions/add_to_cart.php
@@ -1,4 +1,22 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+
 <?php
 include_once(dirname(__DIR__, 1) . '/controllers/cart_controller.php');
 include_once(dirname(__DIR__, 1) . '/settings/core.php');
