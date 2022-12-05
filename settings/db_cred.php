cfa019c9ac387219c61b7a78a87ac7f43057d092
CORNELIUS APAALSE <cornelius.apaalse@ashesi.edu.gh>
2022-12-05 19:26:40 +0000

    Order Details

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,15 +1,15 @@
 <?php
 //Database credentials
 
-define ("SERVER", "localhost");
-define ("PASSWORD","");
-define ("DATABASE","lapam");
-define ("USERNAME","root");
+// define ("SERVER", "localhost");
+// define ("PASSWORD","");
+// define ("DATABASE","lapam");
+// define ("USERNAME","root");
 
 
 // Heroku
-// define ("SERVER", "us-cdbr-east-06.cleardb.net");
-// define ("PASSWORD","3b6f461b");
-// define ("DATABASE","heroku_2171840397ac25e");
-// define ("USERNAME","bf6467ba761b1b");
+define ("SERVER", "us-cdbr-east-06.cleardb.net");
+define ("PASSWORD","3b6f461b");
+define ("DATABASE","heroku_2171840397ac25e");
+define ("USERNAME","bf6467ba761b1b");
 ?>
